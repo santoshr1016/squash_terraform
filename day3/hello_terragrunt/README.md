758e79dfae489a1a423baae8e163ba7bf07ef3eb
santoshr1016 <santy1016@gmail.com>
2020-04-22 12:33:00 +0800

    Add DRY terragrunt

---
+++ day3/hello_terragrunt/README.md
@@ -0,0 +1,6 @@
+### Motivation for writing DRY IsC
+```text
+This work is inspired from terragrunt.io
+The code sample are borrowed from there.
+
+```
--- day3/hello_terragrunt/asg_implementation/vars.tf
+++ day3/hello_terragrunt/asg_implementation/vars.tf
@@ -1,42 +1,35 @@
 variable "aws_region" {
   description = "The AWS region to deploy to (e.g. us-east-1)"
   type        = string
-  default = "ap-southeast-1"
 }
 
 variable "name" {
   description = "The name for the ASG. This name is also used to namespace all the other resources created by this module."
   type        = string
-  default = "terragrunt-asg"
 }
 
 variable "instance_type" {
   description = "The type of EC2 Instnaces to run in the ASG (e.g. t2.micro)"
   type        = string
-  default = "t2.micro"
 }
 
 variable "min_size" {
   description = "The minimum number of EC2 Instances to run in the ASG"
   type        = number
-  default = 2
 }
 
 variable "max_size" {
   description = "The maximum number of EC2 Instances to run in the ASG"
   type        = number
-  default = 2
 }
 
 variable "server_port" {
   description = "The port number the web server on each EC2 Instance should listen on for HTTP requests"
   type        = number
-  default = 8080
 
 }
 
 variable "elb_port" {
   description = "The port number the ELB should listen on for HTTP requests"
   type        = number
-  default = 80
 }
