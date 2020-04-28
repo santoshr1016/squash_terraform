6b02009a6cad7a908a169dde75aba0a282979674
santoshr1016 <santy1016@gmail.com>
2020-04-28 08:46:22 +0800

    :)

--- day1/0_using_vars/using_vars.tf
+++ day1/0_using_vars/using_vars.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 variable "tag_map" {
-  type = "map"
+  type = map(string)
   default = {
     dev = "dev-queue",
     test = "test-queue",
@@ -21,13 +21,55 @@ variable "tag_map" {
   }
 }
 
+variable "zone_name" {
+  default = "helsinki1"
+}
+variable "zones" {
+    type = map(string)
+    default = {
+        "amsterdam" = "nl-ams1"
+        "london"    = "uk-lon1"
+        "frankfurt" = "de-fra1"
+        "helsinki1" = "fi-hel1"
+        "helsinki2" = "fi-hel2"
+        "chicago"   = "us-chi1"
+        "sanjose"   = "us-sjo1"
+        "singapore" = "sg-sin1"
+    }
+}
+output "zone" {
+  value = var.zones["sanjose"]
+}
+
+output "zone_lookup" {
+  value = lookup(var.zones, var.zone_name)
+}
+
+variable "templates" {
+    type = map(string)
+    default = {
+        "ubuntu18" = "01000000-0000-4000-8000-000030080200"
+        "centos7"  = "01000000-0000-4000-8000-000050010300"
+        "debian9"  = "01000000-0000-4000-8000-000020040100"
+    }
+}
+
+variable "plans" {
+    type = map(string)
+    default = {
+        "5USD"   = "1xCPU-1GB"
+        "10uSD"  = "1xCPU-2GB"
+        "20USD"  = "2xCPU-4GB"
+    }
+}
+
 variable "env_type" {}
 variable "queue_name" {}
 
-resource "aws_sqs_queue" "queue" {
-  name = var.queue_name
-
-  tags = {
-    environment_type = lookup(var.tag_map, var.env_type)
-  }
-}
+//resource "aws_sqs_queue" "queue" {
+//  name = var.queue_name
+//
+//  tags = {
+//    environment_type = lookup(var.tag_map, var.env_type)
+//  }
+//}
